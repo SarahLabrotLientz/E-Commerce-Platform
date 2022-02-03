93be0fdca3d395d33ce0751049817d54ea23d2d1
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-02-03 13:49:17 -0500

    working in routes folder specifically updating the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,19 +3,52 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+     // finds all categories
+  // includes its associated Products
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ 
 });
 
+ //This finds one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],   // and includes its associated Products
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Please try again, no category found with that id' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//Created a new category
 router.post('/', (req, res) => {
-  // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.name
+    })
+    res.status(200).json(categoryData)
+  } catch(err) {
+      console.log(err);
+      res.status(400).json({ message: "an error occured", err: err });
+    };
 });
+
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
@@ -26,3 +59,37 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+//reference code begins -------------------------------------------------
+const router = require('express').Router();
+const { Driver, License, Car } = require('../../models');
+
+// GET all drivers
+router.get('/', async (req, res) => {
+  try {
+    const driverData = await Driver.findAll({
+      include: [{ model: License }, { model: Car }],
+    });
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single driver
+router.get('/:id', async (req, res) => {
+  try {
+    const driverData = await Driver.findByPk(req.params.id, {
+      include: [{ model: License }, { model: Car }],
+    });
+
+    if (!driverData) {
+      res.status(404).json({ message: 'No driver found with that id!' });
+      return;
+    }
+
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});

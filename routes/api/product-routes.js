daff171e0ea9325a0060d5ff8331221ecece58ba
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-02-05 13:47:53 -0500

    started on tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,7 +114,7 @@ router.put("/:id", async (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // This deletes one product by its `id` value
   try {
     const productData = await Product.destroy({
